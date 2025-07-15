8d63fa57608e057e9ef989808ab8105d870c5ec6
ANAND PANDEY <anandpandeylive12@gmail.com>
2025-07-15 19:59:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,12 @@ app.use("/api/auth", authRouter )
 app.use("/api/user", userRouter )
 app.use("/api/listing",listingRouter )
 app.use("/api/booking",bookingRouter )
+app.get('/',(req,res)=>{
+    res.send({
+        activeStatus:true,
+        error:false
+    })
+})
 
 
 app.listen(port,()=>{
